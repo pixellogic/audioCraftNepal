85ec1d5f35e0912d536a2f180aa87a8a369b6e0c
Ashish Bajracharya <lsr.files1@gmail.com>
2021-01-24 11:52:05 +0545

    user registration handled

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const SALT_I = 10
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 const userSchema = mongoose.Schema({
     email:{
         type: String,
@@ -40,22 +42,7 @@ const userSchema = mongoose.Schema({
     }
 })
 
-userSchema.pre('save', function(next){
-    var user = this
-    if(user.isModified('password')){
-        bcrypt.genSalt(SALT_I, function(err, salt){
-            if(err) return next(err)
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if(err) return next(err)
-                user.password = hash
-                next()
-            })
-        })
-    }else{
-        next()
-    }
-   
-})
+
 
 const User = mongoose.model('User', userSchema)
 module.exports = {User}
